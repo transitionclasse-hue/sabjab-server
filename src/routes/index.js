4da6a1146e8b48f19b7db63b8094f12195cd36db
transitionclasse-hue <transitionclasse@gmail.com>
2026-02-17 04:01:48 +0530

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,19 @@
 import { authRoutes } from "./auth.js";
 import { orderRoutes } from "./order.js";
 import { categoryRoutes, productRoutes } from "./products.js";
-import { googleConfigRoutes } from "./googleConfig.js"; // ⭐ NEW
+import { googleConfigRoutes } from "./googleConfig.js";
+import { addressRoutes } from "./address.js"; // ✅ NEW
 
 const prefix = "/api";
 
 export const registerRoutes = async (fastify) => {
-    fastify.register(authRoutes, { prefix: prefix });
-    fastify.register(productRoutes, { prefix: prefix });
-    fastify.register(categoryRoutes, { prefix: prefix });
-    fastify.register(orderRoutes, { prefix: prefix });
+  fastify.register(authRoutes, { prefix });
+  fastify.register(productRoutes, { prefix });
+  fastify.register(categoryRoutes, { prefix });
+  fastify.register(orderRoutes, { prefix });
 
-    // ⭐ NEW ROUTE
-    fastify.register(googleConfigRoutes, { prefix: prefix });
+  fastify.register(googleConfigRoutes, { prefix });
+
+  // ✅ ADDRESS ROUTE
+  fastify.register(addressRoutes, { prefix });
 };
